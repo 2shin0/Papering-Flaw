d7b7f2b915ec2e1b16960527e9fa24ec2f00dcf9
Shin Young Lee <02.shin.00@gmail.com>
2024-05-08 18:17:07 +0900

    Update model_train.py

--- model_train.py
+++ model_train.py
@@ -7,31 +7,26 @@ import numpy as np
 from sklearn.metrics import f1_score
 import torch.optim as optim
 
-
 def train_h(model, optimizer, train_loader, val_loader, scheduler, device, patience=3):
     model.to(device)
     criterion = nn.CrossEntropyLoss().to(device)
-    
     best_score = 0
     best_model = None
     
     for epoch in range(1, CFG['EPOCHS']+1):
         model.train()
         train_loss = []
+        
         for imgs, labels in tqdm.tqdm(train_loader):
             imgs = imgs.float().to(device)
             labels = labels.long().to(device)
-            
             optimizer.zero_grad()
-            
             output = model(imgs)
             loss = criterion(output, labels)
-            
             loss.backward()
             optimizer.step()
-            
             train_loss.append(loss.item())
-                    
+            
         _val_loss, _val_score = validation(model, criterion, val_loader, device)
         _train_loss = np.mean(train_loss)
         print(f'Epoch [{epoch}], Train Loss : [{_train_loss:.5f}] Val Loss : [{_val_loss:.5f}] Val Weighted F1 Score : [{_val_score:.5f}]')
@@ -52,9 +47,6 @@ def train_h(model, optimizer, train_loader, val_loader, scheduler, device, patie
     
     return best_model
 
-
-
-
 def validation(model, criterion, val_loader, device):
     model.eval()
     val_loss = []
@@ -64,14 +56,10 @@ def validation(model, criterion, val_loader, device):
         for imgs, labels in tqdm.tqdm(val_loader):
             imgs = imgs.float().to(device)
             labels = labels.long().to(device)
-            
             pred = model(imgs)
-            
             loss = criterion(pred, labels)
-            
             preds += pred.argmax(1).detach().cpu().numpy().tolist()
             true_labels += labels.detach().cpu().numpy().tolist()
-            
             val_loss.append(loss.item())
         
         _val_loss = np.mean(val_loss)
