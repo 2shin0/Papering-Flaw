79b6abdf79bc590d5215af10d91b825288aa8c8a
Shin Young Lee <02.shin.00@gmail.com>
2024-05-08 18:15:40 +0900

    Update main.py

--- main.py
+++ main.py
@@ -9,20 +9,9 @@ def main():
     device = torch.device('cpu') if torch.cuda.is_available() else torch.device('cpu')
     train2, val, le = make_train_df()
     test = make_test_df()
-
     train_loader, val_loader, test_loader =  make_loader(train2, val, test, train_transform, test_transform)
-
     best_model = model_make(train_loader, val_loader, device)
-
-
     preds = inference(le, best_model, test_loader, device)
-
     submit = submit_file(preds)
-
+    
 main()
-
-
-
-
-
-
